07b622e81ad8b24787b32b8dcfff445861cd0b86
landonf <landonf@41597e8f-4b49-0410-a850-dde43e08a396>
2006-10-05 00:25:54 +0000

    Fancy pants unit test framework, and tests for createBucket()

---
+++ java/src/java/com/threerings/s3/AWSAuthConnectionTest.java
@@ -0,0 +1,62 @@
+//  This software code is made available "AS IS" without warranties of any
+//  kind.  You may copy, display, modify and redistribute the software
+//  code either by itself or as incorporated into your code; provided that
+//  you do not remove any proprietary notices.  Your use of this software
+//  code is at your own risk and you waive any claim against Amazon
+//  Digital Services, Inc. or its affiliates with respect to your use of
+//  this software code.
+//
+// (c) 2006 Three Rings Design, Inc.
+// (c) 2006 Amazon Digital Services, Inc. or its affiliates.
+
+package com.threerings.s3;
+
+
+import java.net.HttpURLConnection;
+import junit.framework.TestCase;
+
+public class AWSAuthConnectionTest extends TestCase
+{
+    public AWSAuthConnectionTest (String name)
+    {
+        super(name);
+    }
+    
+    public void setUp ()
+    {
+        _awsId = System.getProperty("aws.id");
+        _awsKey = System.getProperty("aws.key");
+        _conn = new AWSAuthConnection(_awsId, _awsKey, false);
+        _testBucketName = "test-" + _awsId;
+    }
+
+    public void testCreateBucket ()
+        throws Exception
+    {
+        Response response;
+        
+        response = _conn.createBucket(_testBucketName, null);
+        assertEquals("Couldn't create bucket: " +
+            response.connection.getResponseMessage() + ".",
+            HttpURLConnection.HTTP_OK,
+            response.connection.getResponseCode());
+        
+        response = _conn.deleteBucket(_testBucketName, null);
+        assertEquals("Couldn't delete bucket: " +
+            response.connection.getResponseMessage() + ".",
+            HttpURLConnection.HTTP_NO_CONTENT,
+            response.connection.getResponseCode());
+    }
+    
+    /** Amazon S3 Authenticated Connection */
+    protected AWSAuthConnection _conn;
+    
+    /** Amazon Web Services ID */
+    protected String _awsId;
+    
+    /** Amazon Web Services Key */
+    protected String _awsKey;
+    
+    /** Test bucket */
+    protected String _testBucketName;
+}
