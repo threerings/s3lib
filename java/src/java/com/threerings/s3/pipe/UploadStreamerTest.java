608e24c62e03783f4f20999540787020e7202eb4
landonf <landonf@41597e8f-4b49-0410-a850-dde43e08a396>
2007-04-04 21:56:25 +0000

    Checkpoint commit on the upload streamer

---
+++ java/src/java/com/threerings/s3/pipe/UploadStreamerTest.java
@@ -0,0 +1,74 @@
+/* 
+ * UploadStreamer vi:ts=4:sw=4:expandtab:
+ *
+ * Copyright (c) 2005 - 2007 Three Rings Design, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright owner nor the names of contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.threerings.s3.pipe;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+
+public class UploadStreamerTest extends TestCase
+{
+    public UploadStreamerTest (String name) {
+        super(name);
+    }
+
+    public void setUp ()
+        throws Exception
+    {
+        _testFile = File.createTempFile("s3pipe", "data");
+        
+        /* Populate the file with 512k of something */
+        RandomAccessFile file = new RandomAccessFile(_testFile, "rw");
+        file.setLength(512 * 1024);
+    }
+
+    public void tearDown () {
+        _testFile.delete();
+    }
+
+    public void testUpload ()
+        throws Exception
+    {
+        /* Fire up an uploader with a 5k block size. */
+        InputStream input = new FileInputStream(_testFile);
+        UploadStreamer streamer = new UploadStreamer(5*1024);
+        streamer.upload(input);
+    }
+
+    /** Temporary test data file. */
+    protected File _testFile;
+
+    /** Test data stream */
+}
